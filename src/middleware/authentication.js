c8670a67727aa3e2422e8951d918baae3ef79d4a
Tegar <tegardanardana991@gmail.com>
2023-12-18 17:32:26 +0700

    Refactor authentication and error handling middleware

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const handleInvalidToken = (res) => {
   return res.status(401).json({ message: "Access Denied - Invalid Token" });
-};
+}
 
 module.exports = {
-  verifiedToken: async (req, res, next) => {
+  verifiedToken : async (req, res, next) => {
     // Check if the request contains an Authorization header
     const token = req.header("Authorization");
 
@@ -18,25 +18,24 @@ module.exports = {
 
     try {
       const verified = jwt.verify(tokenValue, process.env.JWT_SECRET);
-
-      // Log the token payload
-      console.log("Token Payload:", verified);
-
-      // Assign the verified user to req.user for further use in routes
+      // console.log("Decoded Token:", verified); // Line for debugging
+      // const user = await User.findByPk(verified.id);
       req.user = verified;
+
+      // Add this line to log the token payload
+      console.log("Token Payload:", verified);
+      // console.log("Token Payload:", new Date().getTime());
 
       next();
     } catch (err) {
-      // Pass the error to the errorHandler middleware
-      return next(err);
+      console.error(err);
+      return handleInvalidToken(res);
     }
   },  
-  adminToken: async (req, res, next) => {
-    // Ensure req.user is defined (verifiedToken middleware should be executed first)
-    if (!req.user || req.user.role !== "admin") {
-      // Pass the error to the errorHandler middleware
-      return next({ name: 'ForbiddenError', errorMessage: "Access Denied - You don't have permission" });
-    }
-    next();
+  adminToken : async (req, res, next) => {
+      if (req.user.role !== "admin")
+        return res.status(403).json({ status:'forbidden',message: "Access Denied - You don't have permission" });
+        // throw `user ${verified.id} role is ${verified.role}, not admin`;
+      next();
   }
 };
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,17 +1,17 @@
-const { ValidationError } = require('express-validator');
+const { ValidationError } = require("sequelize");
 
-module.exports.handleError = (error, req, res, next) => {
-    // Log the error for debugging purposes
-    console.error(error);
-
-    // Default status code and error message
+module.exports.handleError = (error, req, res, next ) => {
     let statusCode = 500;
     let errorMessage = 'Internal Server Error';
+    const additional = {};
 
-    // Handle specific error types
     if (error instanceof ValidationError) {
-        statusCode = 422; // Unprocessable Entity
-        errorMessage = error.array().map((validationError) => ({ [validationError.param]: validationError.msg }));
+        statusCode = 422;
+        errorMessage = 'Unprocessable Entity';
+        additional.validationErrors = error.errors.map(validationError => ({
+            field: validationError.path,
+            message: validationError.message,
+        }));
     } else if (error.name === 'SequelizeUniqueConstraintError') {
         statusCode = 409;
         errorMessage = 'Conflict';
@@ -37,9 +37,10 @@ module.exports.handleError = (error, req, res, next) => {
         console.error('Unhandled error:', error);
     }
 
-    // Send the error response to the client
     return res.status(statusCode).json({
         code: statusCode,
-        message: errorMessage
+        message: errorMessage,
+        error: error.message,
+        ...additional,
     });
 };
