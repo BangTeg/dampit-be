c7dcc1b69e488576227de29bbbc487b6740f27e5
Tegar <tegardanardana991@gmail.com>
2023-12-30 17:23:32 +0700

    Add vehicle route to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();    
 const authRoute = require('./authRoute');
 const userRoute = require('./userRoute');
+const vehicleRoute = require('./vehicleRoute');
 const {verifiedToken} = require('../middlewares/authentication');    // Import the verifiedToken middleware
 
 // Route to the auth route
@@ -9,5 +10,8 @@ router.use('/auth', authRoute);
 
 // Route to the user router
 router.use('/user', verifiedToken, userRoute);
+
+// Route to the vehicle router
+router.use('/vehicle', verifiedToken, vehicleRoute);
 
 module.exports = router;
