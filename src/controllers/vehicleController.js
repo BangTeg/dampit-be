1e87bbe46804892c5134c0ff8de8cd190fa459f1
Tegar D. Lokananta <tegarlokananta.dev@gmail.com>
2024-01-23 23:28:50 +0700

    Add user and reservation information to vehicleController

--- src/controllers/vehicleController.js
+++ src/controllers/vehicleController.js
@@ -1,15 +1,34 @@
-const { Vehicles, Reservations } = require('../../db/models');
+const { Vehicles, Reservations, Users } = require('../../db/models');
 const { Op } = require('sequelize');
 const { crudController } = require('../utils/crud');
 const { handleError } = require('../middlewares/errorHandler');
 
 const defaultPageLimit = 10;
 
+const attributes = ["id", "name", "price", "model", "capacity", "include", "area", "parking", "payment", "overtime", "createdAt", "updatedAt"];
+
+const includeUser = {
+    model: Users,
+    as: 'Users',
+    attributes: ["id", "username", "firstName", "lastName", "email", "role", "gender", "avatar", "address", "contact", "ktp"],
+};
+
+const includeReservation = {
+    model: Reservations,
+    as: 'Reservations',
+    attributes: ["id", "userId", "vehicleId", "pickUp", "dropOff", "passengers", "institution", "unit", "pickDate", "dropDate", "status", "totalPrice", "finishedAt", "createdAt", "updatedAt"],
+};
+
+const include = [includeUser, includeReservation];
+
 module.exports = {
+    includeUser,
+    includeReservation,
+    attributes,
     getAll : async (req, res) => {
-        const filter = {};
         return await crudController.getAll(Vehicles, {
-            where: filter,
+            where: {},
+            include,
             paginated: true,
         })(req, res);
     },
@@ -46,8 +65,47 @@ module.exports = {
         }
     },
 
-    getById: crudController.getById(Vehicles),
+    getById: crudController.getById(Vehicles, {
+        include,
+    }),
+
     create: crudController.create(Vehicles),
-    update: crudController.update(Vehicles),
-    delete: crudController.delete(Vehicles),
+
+    update: crudController.update(Vehicles, {
+        include,
+    }),
+
+    delete: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            // Fetch the vehicle along with its reservations
+            const vehicle = await Vehicles.findByPk(id, {
+            include: [includeReservation],
+            });
+
+            if (!vehicle) {
+            const ret = {
+                code: 404,
+                status: "Not Found",
+                message: `Vehicle not found, finding ${id}`,
+            };
+            
+            return res.status(404).json(ret);
+            }
+
+            // Delete the vehicle and its associated reservations
+            await vehicle.destroy({ include: [Reservations] });
+
+            const ret = {
+                code: 200,
+                status: "OK",
+                message: `Success deleting ${Vehicles.name} and associated reservations`,
+            };
+
+            return res.status(200).json(ret);
+        } catch (err) {
+            return handleError(res, err);
+        }
+        },
 };
